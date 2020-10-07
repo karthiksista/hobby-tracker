4dfb8eb4aae5d3d4aecad5e02b81f3a341e83696
karthiksista <karthik.sista6@gmail.com>
2020-10-07 09:56:25 +0530

    send email on registration.

--- src/components/SignUp/Signup.js
+++ src/components/SignUp/Signup.js
@@ -5,6 +5,11 @@ import app from '../../base';
 import { useFormik } from 'formik';
 
 const Signup = ({ history }) => {
+  var actionCodeSettings = {
+    url: 'https://karthiksista.netlify.app/',
+
+    handleCodeInApp: true,
+  };
   const formik = useFormik({
     initialValues: {
       email: '',
@@ -13,15 +18,23 @@ const Signup = ({ history }) => {
     onSubmit: (values) => {
       handleSignUpSubmit(values);
     },
-    validate: value => {
-      
-    }
+    validate: (value) => {},
   });
+
   const handleSignUpSubmit = useCallback(
     async (values) => {
       const { email, password } = values;
       try {
         await app.auth().createUserWithEmailAndPassword(email, password);
+
+        try {
+          await app
+            .auth()
+            .currentUser.sendEmailVerification(actionCodeSettings);
+          // console.log('sent');
+        } catch (error) {
+          console.log(error, 'error');
+        }
         history.push('/');
       } catch (error) {
         alert(error);
